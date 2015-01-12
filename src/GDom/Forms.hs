88d595beef60a6c6aee08ab943514bd38c127e64
Артур Файзрахманов <heraldhoi@gmail.com>
2015-01-13 01:45:15 +0500

    добавлен модуль для взаимодействия с формами и их элементами

---
+++ src/GDom/Forms.hs
@@ -0,0 +1,47 @@
+module GDom.Forms (
+  inputType
+, inputTypeSafe
+, inputTypeMT
+) where
+
+import           Control.Monad                (when)
+import           Control.Monad.Trans          (lift,liftIO)
+import           Control.Monad.Trans.Maybe    (MaybeT(..))
+import           Data.Text                    (Text)
+import qualified Data.Text                    as T
+import           GHCJS.Foreign
+import           GHCJS.Marshal
+import           GHCJS.Types
+import           GDom.CommonDom               (tagName)
+import           GDom.Types
+
+--------------------------------------------------------------------------------
+inputType :: DocumentElement -> IO Text
+inputType e = return . fromJSString =<< js_inputType e
+
+inputTypeSafe :: DocumentElement -> IO (Maybe InputType)
+inputTypeSafe e = do
+    tnam <- tagName e
+    when (tnam /= InputTagName)
+            (error "inputTypeSafe shoould be used with input elements only.")
+    ityp <- js_inputType e
+    fromJSRef (castRef ityp)
+
+inputTypeMT :: DocumentElement -> MaybeT IO InputType
+inputTypeMT e = tNameCheck >>= getType
+    where
+        tNameCheck = do
+            tname <- liftIO . tagName $ e
+            if tname /= InputTagName
+                then MaybeT . return $ Nothing
+                else return ()
+        getType x = do
+            typ <- liftIO . js_inputType $ e
+            MaybeT . fromJSRef . castRef $ typ
+--------------------------------------------------------------------------------
+
+
+--------------------------------------------------------------------------------
+foreign import javascript safe "$r = $1.type;"
+    js_inputType :: DocumentElement -> IO JSString
+--------------------------------------------------------------------------------
